549223ff2a40c51fe16a1977ca78af6831bfd8d1
Simonlamb <yabiange@vip.163.com>
2022-01-17 19:56:47 +0800

    添加数据:battlemaster_entry

---
+++ sql/custom/world/2022_01_17_04_world_battlemaster_entry.sql
@@ -0,0 +1,18 @@
+
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (18439, 6);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (32169, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (32170, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (34976, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35596, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35597, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35598, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35599, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35600, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35601, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35602, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35603, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35611, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (35612, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (49573, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (415103, 32);
+INSERT IGNORE `battlemaster_entry`(`entry`, `bg_template`) VALUES (415106, 32);
