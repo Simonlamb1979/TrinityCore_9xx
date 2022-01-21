1e832cb8ef7804d63de7a458ec6ff35d4e524d49
Simonlamb <yabiange@vip.163.com>
2022-01-21 16:25:22 +0800

    修复控制台错误提示:npc_text

---
+++ sql/custom/world/2022_01_21_04_world_DBErrors_npc_text.sql
@@ -0,0 +1,3 @@
+UPDATE `npc_text` SET `BroadcastTextID0` = 50429 WHERE `ID` IN (2953, 2955, 2956, 2957, 7114, 8321, 8332, 10128, 11128, 13157, 14330);
+UPDATE `npc_text` SET `BroadcastTextID1` = 50429 WHERE `ID` = 10843;
+UPDATE `npc_text` SET `BroadcastTextID1` = 50429, `BroadcastTextID2` = 50429, `BroadcastTextID3` = 50429, `BroadcastTextID4` = 50429, `BroadcastTextID5` = 50429, `BroadcastTextID6` = 50429, `BroadcastTextID7` = 50429 WHERE `ID` = 11428;
