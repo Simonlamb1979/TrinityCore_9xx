70cce9745d61e0b979dc2817473546777c37396d
Simonlamb <yabiange@vip.163.com>
2022-01-22 17:59:02 +0800

    修复控制台错误提示:gameobject_questitem

--- sql/custom/world/2022_01_22_02_world_DBErrors_gameobject_template.sql
+++ sql/custom/world/2022_01_22_02_world_DBErrors_gameobject_template.sql
@@ -3,3 +3,4 @@ UPDATE `gameobject_template` SET `displayId` = 65816 WHERE `entry` = 175885;
 UPDATE `gameobject_template` SET `displayId` = 12840 WHERE `entry` = 185324;
 UPDATE `gameobject` SET `PhaseId` = 0 WHERE `guid` = 32360;
 DELETE FROM `gameobject` WHERE `guid` = 500063;
+UPDATE `gameobject` SET `PhaseId` = 0 WHERE `guid` = 21065;
