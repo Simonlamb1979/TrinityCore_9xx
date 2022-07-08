779e985bb34b0f1853a2cf7856c6539a7f59dd2f
Simonlamb <yabiange@vip.163.com>
2022-07-09 07:58:09 +0800

    修复控制台错误提示:quest_template第一部分

---
+++ sql/custom/world/2022_07_09_02_world_quest_details.sql
@@ -0,0 +1,4 @@
+UPDATE `quest_details` SET `Emote1` = 1 WHERE `Emote1` > 744;
+UPDATE `quest_details` SET `Emote2` = 1 WHERE `Emote2` > 744;
+UPDATE `quest_details` SET `Emote3` = 1 WHERE `Emote3` > 744;
+UPDATE `quest_details` SET `Emote4` = 1 WHERE `Emote4` > 744;
