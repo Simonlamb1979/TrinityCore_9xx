caadb47c32576dff07c556635f5c645d770fa431
Simonlamb <yabiange@vip.163.com>
2022-07-09 06:47:50 +0800

    修复控制台错误提示:quest_reward_display_spell

---
+++ sql/custom/world/2022_07_09_01_world_quest_reward_display_spell.sql
@@ -0,0 +1,5 @@
+UPDATE `quest_reward_display_spell` SET `PlayerConditionID` = 0 WHERE `QuestID` = 58160 AND `SpellID` = 339041;
+UPDATE `quest_reward_display_spell` SET `PlayerConditionID` = 0 WHERE `QuestID` = 61058 AND `SpellID` = 345643;
+UPDATE `quest_reward_display_spell` SET `PlayerConditionID` = 0 WHERE `QuestID` = 63732 AND `SpellID` = 357930;
+UPDATE `quest_reward_display_spell` SET `PlayerConditionID` = 0 WHERE `QuestID` = 64216 AND `SpellID` = 354850;
+DELETE FROM `quest_reward_display_spell` WHERE `QuestID` = 62828 AND `SpellID` = 344819;
