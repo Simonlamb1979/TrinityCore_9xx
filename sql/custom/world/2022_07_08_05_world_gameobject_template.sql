5fe0237df3606174f4a94efb4e823280d8176d34
Simonlamb <yabiange@vip.163.com>
2022-07-08 18:47:04 +0800

    修复控制台错误提示:gameobject_template

---
+++ sql/custom/world/2022_07_08_05_world_gameobject_template.sql
@@ -0,0 +1,92 @@
+UPDATE `gameobject_template` SET `Data1` = 0 WHERE `entry` IN (210783, 210784, 210785, 210786, 210787, 210788, 210789, 210800, 210862, 210863, 210864, 210866, 210867, 211133, 211134, 211135, 211136, 211137, 211138, 211245, 211246, 211258, 211293, 211614, 211615, 211844, 211963, 211970, 211976, 211977, 211981, 211982, 212160, 212244, 212245, 212246, 212265, 212266, 212921, 212939, 212940, 212941, 212942, 212982, 212983, 212984, 212985, 212986, 213085, 213090, 213091, 213172, 213194, 213196, 213197, 213244, 213258, 213268, 213269, 213270, 213271, 213285, 213373, 213395, 213396, 213397, 213398, 213544, 213545, 213547, 213548, 213549, 213550, 213593, 213594, 213595, 213596, 213597, 213638, 213639, 213665, 213666, 213903, 214261, 214401, 214402, 214483, 214484, 214485, 214486, 214487, 214488, 214489, 214490, 214491, 214492, 214493, 214526, 214641, 214696, 214887, 214888, 215130, 215363, 215364, 215365, 215366, 215367, 215368, 215369, 215370, 215371, 215372, 215375, 215376, 215379, 215380, 215381, 215382, 215391, 215459, 215760, 215787, 216003, 216004, 216005, 216006, 216007, 216008, 216009, 216060, 216354, 216355, 216356, 216357, 216358, 216359, 216698, 216699);
+UPDATE `gameobject_template` SET `Data0` = 0 WHERE `entry` IN (210791, 210792, 210793, 210794, 211442, 212325, 212582, 212587, 212594, 212595, 212745, 212906, 213475, 213481, 213489, 213497, 213866, 213867, 213868, 213869, 213870, 213871, 213872, 213873, 213874, 213875, 213876, 213877, 213878, 213879, 213957, 215017, 215022, 215029, 215030, 215177, 215178, 215179, 215180, 215383, 215384, 215385, 215386);
+UPDATE `gameobject_template` SET `Data0` = 166879 WHERE `entry` = 181102;
+UPDATE `gameobject_template` SET `Data0` = 57651 WHERE `entry` = 181105;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 181106;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 181165;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 183510;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 183511;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 188597;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 188598;
+UPDATE `gameobject_template` SET `Data0` =42955 WHERE `entry` = 193061;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 193169;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 193170;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 193171;
+UPDATE `gameobject_template` SET `Data0` =81349 WHERE `entry` = 204422;
+UPDATE `gameobject_template` SET `Data0` =123071 WHERE `entry` = 205272;
+UPDATE `gameobject_template` SET `Data0` =121852 WHERE `entry` = 205273;
+UPDATE `gameobject_template` SET `Data0` =66238 WHERE `entry` = 205876;
+UPDATE `gameobject_template` SET `Data0` =66238 WHERE `entry` = 205877;
+UPDATE `gameobject_template` SET `Data0` =86855 WHERE `entry` = 206195;
+UPDATE `gameobject_template` SET `Data0` =9175 WHERE `entry` = 207073;
+UPDATE `gameobject_template` SET `Data0` =9175 WHERE `entry` = 207078;
+UPDATE `gameobject_template` SET `Data0` =221694 WHERE `entry` = 207690;
+UPDATE `gameobject_template` SET `Data0` =85040 WHERE `entry` = 207691;
+UPDATE `gameobject_template` SET `Data0` =220485 WHERE `entry` = 208325;
+UPDATE `gameobject_template` SET `Data0` =121857 WHERE `entry` = 209080;
+UPDATE `gameobject_template` SET `Data0` =123073 WHERE `entry` = 209081;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 214351;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 214722;
+UPDATE `gameobject_template` SET `Data0` =84505 WHERE `entry` = 227831;
+UPDATE `gameobject_template` SET `Data0` =173162 WHERE `entry` = 231620;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 239423;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 239424;
+UPDATE `gameobject_template` SET `Data0` =162460 WHERE `entry` = 241121;
+UPDATE `gameobject_template` SET `Data0` =137279 WHERE `entry` = 245623;
+UPDATE `gameobject_template` SET `Data0` =215411 WHERE `entry` = 251521;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 251858;
+UPDATE `gameobject_template` SET `Data0` =241140 WHERE `entry` = 267034;
+UPDATE `gameobject_template` SET `Data0` =57651 WHERE `entry` = 339222;
+UPDATE `gameobject_template` SET `Data0` =319632 WHERE `entry` = 344767;
+
+DELETE FROM `page_text` WHERE `ID` IN (6279, 6881, 7618, 7803, 7817, 7859, 7880, 7955, 7979, 7980, 7988, 7990, 8065, 8203, 8104, 8212, 8211, 8210, 8209, 8208, 8205, 8157, 8158, 8159, 8160, 8162, 8163, 8164, 8165, 8166, 8167, 8168, 8169, 8170, 8171, 8183, 8240, 8328, 8399, 8398, 8393);
+INSERT INTO `page_text`(`ID`, `Text`) VALUES 
+(6279, '你好。此文字暂时缺失。'),
+(6881, '你好。此文字暂时缺失。'),
+(7618, '你好。此文字暂时缺失。'),
+(7803, '你好。此文字暂时缺失。'),
+(7817, '你好。此文字暂时缺失。'),
+(7859, '你好。此文字暂时缺失。'),
+(7880, '你好。此文字暂时缺失。'),
+(7955, '你好。此文字暂时缺失。'),
+(7979, '你好。此文字暂时缺失。'),
+(7980, '你好。此文字暂时缺失。'),
+(7988, '你好。此文字暂时缺失。'),
+(7990, '你好。此文字暂时缺失。'),
+(8065, '你好。此文字暂时缺失。'),
+(8203, '你好。此文字暂时缺失。'),
+(8104, '你好。此文字暂时缺失。'),
+(8212, '你好。此文字暂时缺失。'),
+(8211, '你好。此文字暂时缺失。'),
+(8210, '你好。此文字暂时缺失。'),
+(8209, '你好。此文字暂时缺失。'),
+(8208, '你好。此文字暂时缺失。'),
+(8205, '你好。此文字暂时缺失。'),
+(8157, '你好。此文字暂时缺失。'),
+(8158, '你好。此文字暂时缺失。'),
+(8159, '你好。此文字暂时缺失。'),
+(8160, '你好。此文字暂时缺失。'),
+(8162, '你好。此文字暂时缺失。'),
+(8163, '你好。此文字暂时缺失。'),
+(8164, '你好。此文字暂时缺失。'),
+(8165, '你好。此文字暂时缺失。'),
+(8166, '你好。此文字暂时缺失。'),
+(8167, '你好。此文字暂时缺失。'),
+(8168, '你好。此文字暂时缺失。'),
+(8169, '你好。此文字暂时缺失。'),
+(8170, '你好。此文字暂时缺失。'),
+(8171, '你好。此文字暂时缺失。'),
+(8183, '你好。此文字暂时缺失。'),
+(8240, '你好。此文字暂时缺失。'),
+(8328, '你好。此文字暂时缺失。'),
+(8399, '你好。此文字暂时缺失。'),
+(8398, '你好。此文字暂时缺失。'),
+(8393, '你好。此文字暂时缺失。');
+
+UPDATE `gameobject_template` SET `Data0` = 215782 WHERE `entry` IN (185316, 186810, 204819, 205253, 207282, 207283, 207284, 207285, 207326, 207350, 212347, 213484, 225786, 227831, 241121, 242404, 242405, 249852, 266306, 293344, 293551, 344767, 365609, 368216, 369256, 373592);
+UPDATE `gameobject_template` SET `Data0` = 93 WHERE `entry` IN (202357, 73, 353850, 364869, 364871, 364873, 364875);
+UPDATE `gameobject_template` SET `type` = 6 WHERE `entry` = 188187;
+UPDATE `gameobject_template` SET `type` = 15, `VerifiedBuild` = 18019 WHERE `entry` = 177233;
+DELETE FROM `transports` WHERE `guid` = 8;
+
+DELETE FROM `gameobject_template_addon` WHERE `entry` IN (251015, 311580);
