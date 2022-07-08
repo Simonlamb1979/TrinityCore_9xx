4d51559e2a7d07b2771b3f884a023b1b48a7fc45
Simonlamb <yabiange@vip.163.com>
2022-07-09 06:41:36 +0800

    修复控制台错误提示:linked_respawn

---
+++ sql/custom/world/2022_07_09_00_world_linked_respawn.sql
@@ -0,0 +1,6 @@
+DELETE FROM `linked_respawn` WHERE `guid` = 125913 AND `linkedGuid` = 126026;
+DELETE FROM `linked_respawn` WHERE `guid` = 126091 AND `linkedGuid` = 126102;
+DELETE FROM `linked_respawn` WHERE `guid` = 126092 AND `linkedGuid` = 126102;
+DELETE FROM `linked_respawn` WHERE `guid` = 126093 AND `linkedGuid` = 126102;
+DELETE FROM `linked_respawn` WHERE `guid` = 126094 AND `linkedGuid` = 126102;
+DELETE FROM `linked_respawn` WHERE `guid` = 127005 AND `linkedGuid` = 127043;
