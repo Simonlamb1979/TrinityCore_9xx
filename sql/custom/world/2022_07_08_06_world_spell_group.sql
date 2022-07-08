e79d303510a669a3299c56853e29d6cf386215bf
Simonlamb <yabiange@vip.163.com>
2022-07-08 18:56:44 +0800

    修复控制台错误提示:spell_group

---
+++ sql/custom/world/2022_07_08_06_world_spell_group.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_group` WHERE `spell_id` IN (17629, 42735, 62380, 67016, 67017, 67018, 2380);
+DELETE FROM `spell_proc` WHERE `spellid` IN (20784, 33127);
+DELETE FROM `spell_threat` WHERE `entry` = 32546;
+DELETE FROM `spell_group_stack_rules` WHERE `group_id` = 2500;
