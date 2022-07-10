5cf758dbd6c6c7110150aa7794dc0b5aa5f1d128
Simonlamb <yabiange@vip.163.com>
2022-07-10 11:55:57 +0800

    修复控制台错误提示:creature_formations

---
+++ sql/custom/world/2022_07_10_04_world_creature_formations.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature_formations` WHERE `leaderGUID` IN (13166, 15424, 26213, 26233, 37385, 38429, 45501, 66989, 66992, 120835, 362545, 362590); 
+DELETE FROM `creature_formations` WHERE `memberGUID` = 120835; 
